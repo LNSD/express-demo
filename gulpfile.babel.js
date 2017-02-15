d75cca864727630a0ff0de0296ffe5ca2d9d9b4b
Lorenzo Delgado <lorenzodelgado.dev@gmail.com>
2017-02-15 23:17:46 +0100

    Base express-es6-handlebars project

---
+++ gulpfile.babel.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import babel from 'gulp-babel';
+import sass from 'gulp-sass';
+import del from 'del';
+
+gulp.task('build:babel', function () {
+    return gulp.src('src/app/*.js')
+               .pipe(babel())
+               .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy:views', function () {
+    return gulp.src('src/app/views/**/*')
+               .pipe(gulp.dest('dist/views'));
+});
+
+gulp.task('clean:app', function () {
+    gutil.log('Cleaning:', gutil.colors.red('dist/**/*'));
+    return del('dist/**/*');
+});
+
+gulp.task('clean', ['clean:app']);
+gulp.task('build', ['build:babel', 'copy:views']);
+
+gulp.task('default', ['clean', 'build']);
---
+++ src/app/app.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import express from 'express';
+import bodyparser from 'body-parser';
+import logger from 'morgan';
+import handlebars from 'express-handlebars';
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(bodyparser.json()); // support json encoded bodies
+app.use(bodyparser.urlencoded({ extended: true })); // support encoded bodies
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Register simple HTTP GET route for /
+app.get('/', function(req, res){
+    // Send status 200 and render content. Content, in this case, is a non-existent template.
+    // For me, rendering the layout is important.
+    res.status(200).render('home');
+});
+
+// Set the server port to 3000, and log the message when the server is ready.
+app.listen(3000, function(){
+    console.log('Server is listening at localhost:3000');
+});

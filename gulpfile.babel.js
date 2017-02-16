e8fe972174dbdfac420cdecb092972629ac6502b
Lorenzo Delgado <lorenzodelgado.dev@gmail.com>
2017-02-16 22:42:01 +0100

    Updated develop task: Added livereload and gulp-nodemon tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,16 +4,15 @@ import gulp from 'gulp';
 import seq from 'gulp-sequence';
 import merge from 'merge-stream';
 import del from 'del';
-import _ from 'lodash';
 
 import gutil from 'gulp-util';
-import watch from 'gulp-watch';
 import concat from 'gulp-concat';
 import uglify from 'gulp-uglify';
 import sourcemaps from 'gulp-sourcemaps';
 import babel from 'gulp-babel';
 import sass from 'gulp-sass';
-
+import livereload from 'gulp-livereload';
+import nodemon from 'gulp-nodemon';
 
 gulp.task('clean:dist', function () {
     gutil.log('Cleaning:', gutil.colors.red('dist/**/*'));
@@ -31,7 +30,8 @@ gulp.task('build:app', function () {
 
 gulp.task('copy:views', function () {
     return gulp.src('src/views/**/*')
-        .pipe(gulp.dest('dist/views'));
+        .pipe(gulp.dest('dist/views'))
+        .pipe(livereload());
 });
 
 const vendorSass = [
@@ -43,7 +43,8 @@ gulp.task('build:styles', function () {
                .pipe(sourcemaps.init())
                .pipe(sass({includePaths: vendorSass}).on('error', sass.logError))
                .pipe(sourcemaps.write('.'))
-               .pipe(gulp.dest('dist/assets/css'));
+               .pipe(gulp.dest('dist/assets/css'))
+        .pipe(livereload());
 });
 
 const vendorJS = [
@@ -61,32 +62,42 @@ gulp.task('build:scripts', function () {
         //only uglify if gulp is ran with '--type production'
         .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist/assets/js'));
+        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(livereload());
 });
 
 gulp.task('copy:media', function () {
     return gulp.src('src/media/**/*.{jpg,jpeg,png}')
-        .pipe(gulp.dest('dist/assets/media'));
+        .pipe(gulp.dest('dist/assets/media'))
+        .pipe(livereload());
 });
 
 const vendorFonts = [ './node_modules/font-awesome/fonts/*' ];
 gulp.task('copy:fonts', function () {
     return gulp.src(vendorFonts)
-        .pipe(gulp.dest('dist/assets/fonts'));
+        .pipe(gulp.dest('dist/assets/fonts'))
+        .pipe(livereload());
 });
 
 
 /* Watch these files for changes and run the task on update */
-gulp.task('watch', () => {
+gulp.task('watch', function () {
+    livereload.listen();
+
     gulp.watch('src/app/*.js', ['build:app']);
     gulp.watch('src/views/**/*.{hbs,html}', ['copy:views']);
     gulp.watch('src/styles/*.{scss,sass}', ['build:styles']);
     gulp.watch('src/scripts/*.js', ['build:scripts']);
 });
 
+gulp.task('server', function () {
+    nodemon({
+        'script': 'dist/app.js',
+    });
+});
 
 gulp.task('clean', ['clean:dist']);
 gulp.task('build', ['build:app', 'copy:views', 'build:styles', 'build:scripts', 'copy:media', 'copy:fonts']);
 
-gulp.task('develop', seq('clean', 'build', 'watch'));
+gulp.task('develop', seq('clean', 'build', ['server', 'watch']));
 gulp.task('default', seq('clean', 'build'));
--- src/app/app.js
+++ src/app/app.js
@@ -4,6 +4,7 @@ import bodyparser from 'body-parser';
 import logger from 'morgan';
 import handlebars from 'express-handlebars';
 
+const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(logger('dev'));
@@ -11,10 +12,16 @@ app.use(logger('dev'));
 app.use(bodyparser.json()); // support json encoded bodies
 app.use(bodyparser.urlencoded({ extended: true })); // support encoded bodies
 
-app.use(express.static('assets'));
+app.use(express.static(path.join(__dirname,'assets')));
 
-app.engine('.hbs', handlebars({defaultLayout: 'main', extname: '.hbs'}));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', handlebars({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
 app.set('view engine', '.hbs');
+
 
 // Register simple HTTP GET route for /
 app.get('/', function(req, res) {
@@ -22,12 +29,14 @@ app.get('/', function(req, res) {
     res.status(200).render('index');
 });
 
+// Register simple HTTP POST route for /
 app.post('/', function(req, res){
     res.render('welcome', { name: req.body.name});
 });
 
-// Set the server port to 3000, and log the message when the server is ready.
-app.listen(8080, function() {
+
+// Set the server port to 8080, and log the message when the server is ready.
+app.listen(port, function() {
     console.log('Server is listening at localhost:8080');
 });
 

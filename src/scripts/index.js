fbfcc7e085d956b96011b14dec1cfa8941183164
Lorenzo Delgado <lorenzodelgado.dev@gmail.com>
2017-02-17 01:08:52 +0100

    Added form required fields check

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -36,3 +36,41 @@ $('#birthday').bootstrapBirthday({
         }
     }
 });
+
+$('#submit').click(function(event)
+{
+    if($('#name').val().length == 0) {
+        $('#name').addClass('error');
+        event.preventDefault();
+    }
+    else
+    {
+        $('#name').removeClass('error');
+    }
+
+    if($('[name="birthday[year]"]').val() == 0) {
+        $('[name="birthday[year]"]').addClass('error');
+        event.preventDefault();
+    }
+    else
+    {
+        $('[name="birthday[year]"]').removeClass('error');
+    }
+
+    if($('[name="birthday[month]"]').val() == 0) {
+        $('[name="birthday[month]"]').addClass('error');
+        event.preventDefault();
+    }
+    else
+    {
+        $('[name="birthday[month]"]').removeClass('error');
+    }
+
+    if($('[name="birthday[day]"]').val() == 0) {
+        $('[name="birthday[day]"]').addClass('error');
+        event.preventDefault();
+    }
+    else
+    {
+        $('[name="birthday[day]"]').removeClass('error');
+    }});
